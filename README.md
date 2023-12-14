3c2da52b0571c0bee3d7811fd9937ad85092322d
Stephen Carder <stephen@stafngrimr.com>
2023-12-14 15:52:20 +0000

    Main page added to previous commitx

--- README.md
+++ README.md
@@ -7,5 +7,14 @@ https://simplegametutorials.github.io/love/
 https://extracredits.site
 
 ## What kind of game are we going to make?
-Let's go with something almost modular.. there will be a main point to the game, but there are several features already in mind that we're going to ignore now and maybe add on later.
-The crux of the game is that you are an adventurer, venturing out into the wor
+
+There are going to be 6 rooms.
+
+You wake up disoriented in the first room. You're not sure how you arrived and you think you passed out from being drunk or maybe someone spiked your drink??
+
+You remember getting to the venue and not much else past that.. You look around and you're in the toilet actually. How on earth did I get in here. I think I locked myself in.. 
+
+Better find the key..
+
+(pickup the key)
+Ah, there's the key. I can get out of here finally.
--- main.lua
+++ main.lua
@@ -1,84 +1,119 @@
 function love.load()
-	-- Window settings
-	love.window.setTitle("Castle Stafngrimr")
-	love.window.setMode(1450, 800)
+	-- Modules
+	Object = require "classic"
+	require "player"
 
-	-- Player starter ettings
-	square = {}
-	square.x = 200
-	square.y = 200
-	square.height = 50
-	square.width = 50
+	player = Player()
 
-	-- Map Area
-	map = {}
-	map.x = 100
-	map.y = 100
-	map.height = 600
-	map.width = 600
+        -- Window settings    
+        love.window.setTitle("Metalhead by Stafngrimr")
+        love.window.setMode(1450, 800)
+        font = love.graphics.newFont("img/Courier New.ttf", 20)
 
-	-- imagedump
-	exit = love.graphics.newImage("img/exit.png")
-	north = love.graphics.newImage("img/north.png")
-	west = love.graphics.newImage("img/west.png")
-	east = love.graphics.newImage("img/east.png")
-	south = love.graphics.newImage("img/south.png")
-	playerImage = north
+        -- Map Area     
+        map = {
+                x = 100,
+                y = 100,
+                height = 600,
+                width = 600
+        }
+
+        -- PickupObj    
+        pickup = {
+                x = 400,
+                y = 500,
+                diam = 25,
+		acquired = false
+        }
+
+        -- Text/Menu area    
+        square = {
+                x = 750,
+                y = 100,
+                height = 600,
+                width = 600
+        }
+
+        text = "This is where the menu text is going to go. We're going to leave this in right now as some kind of placeholder, but we'll change it up, with the pick up of an object. That's what's next. Pickup.. \n\nAnd roll\n\n"
+
+        -- imagedump    
+        entranceMap = love.graphics.newImage("img/entrance-bg.png")
+	toiletMap = love.graphics.newImage("img/toilet-bg.png")
 end
 
-function love.update()
-	if love.keyboard.isDown("w") then
-		if love.keyboard.isDown("lshift") then
-			square.y = square.y - 4
-		else
-			square.y = square.y - 2
-		end
-		playerImage = north
-	elseif love.keyboard.isDown("s") then
-		if love.keyboard.isDown("lshift") then
-			square.y = square.y + 4
-		else
-			square.y = square.y + 2
-		end
-		playerImage = south
-	elseif love.keyboard.isDown("a") then
-		if love.keyboard.isDown("lshift") then
-			square.x = square.x - 4
-		else
-			square.x = square.x - 2
-		end
-		playerImage = west
-	elseif love.keyboard.isDown("d") then
-		if love.keyboard.isDown("lshift") then
-			square.x = square.x + 4
-		else
-			square.x = square.x + 2
-		end
-		playerImage = east
+function collisionCheck(firstx, firsty, firstw, firsth, secondx, secondy, secondw, secondh)
+	local obj1_left = firstx
+	local obj1_right = firstx + firstw
+	local obj1_top = firsty local obj1_bottom = firsty + firsth
+
+	local obj2_left = secondx
+	local obj2_right = secondx + secondw
+	local obj2_top = secondy
+	local obj2_bottom = secondy + secondh
+
+	return obj1_right > obj2_left
+		and obj1_left < obj2_right
+		and obj1_bottom > obj2_top
+		and obj1_top < obj2_bottom
+end
+
+function love.update(dt)
+	player:update(dt)
+	
+	-- Interact with Pickup object
+	if collisionCheck(player.x, player.y, player.width, player.height, pickup.x, pickup.y, 25, 25)
+	and pickup.acquired == false then
+		text = text .."You touched the goddamn line! Now I wonder if we can change it based on you not touching the thing.."
+		pickup.acquired = true
 	end
 
-	if square.y < 100 then
-		square.y = square.y + 5
-	elseif square.y > 650 then
-		square.y = square.y - 5
-	elseif square.x < 100 then
-		square.x = square.x + 5
-	elseif square.x > 650 then
-		square.x = square.x - 5
+	-- Move from one room to another
+	if player.position == "entrance" then
+		if collisionCheck(player.x, player.y, player.width, player.height, 100, 575, 1, 75) then
+			player.position = "toilet"
+			player.x = 600
+			player.y = 595
+		end
+	elseif player.position == "toilet" then
+		if collisionCheck(player.x, player.y, player.width, player.height, 699, 575, 1, 75) then
+			player.position = "entrance"
+			player.x = 200
+			player.y = 595
+		end
 	end
 end
 
 function love.draw()
 	-- drawing using position x y, width, height)
 	
+	-- Menustuff
+	function menuText()
+		love.graphics.setFont(font)
+		love.graphics.setColor(1, 0, 0, 0.5)
+		love.graphics.printf(text, 800, 150, 500)
+	end
+
 	-- Map
-	love.graphics.rectangle("line", map.x, map.y, map.width, map.height)
+	if player.position == "entrance" then
+		love.graphics.draw(entranceMap, map.x, map.y)
+	elseif player.position == "toilet" then
+		love.graphics.draw(toiletMap, map.x, map.y)
+	end
+
+	-- Text/Menu Area
+	love.graphics.rectangle("line", square.x, square.y, square.width, square.height)
+
+	-- Pickupobject
+	if pickup.acquired == false then
+		love.graphics.setColor(1, 1, 0, 1)
+		love.graphics.circle("fill", pickup.x, pickup.y, pickup.diam)
+	end
+
+	menuText()
+	love.graphics.setColor(1, 1, 1)
 
 	-- Player
-	love.graphics.draw(playerImage, square.x, square.y)
-
-	-- Exit
-	love.graphics.draw(exit, 1325, 100) -- Can't just put filename here
+	player:draw()
 end
 
 function love.focus(f)
@@ -90,15 +125,14 @@ function love.focus(f)
 end
 
 function love.mousepressed(x, y, button)
-	if button == 1 and x < 1300 and x > 1275 and y < 125 and y > 100 then
-		print("Quit by CROSS")
-		love.event.quit()
-	end
+--	if button == 1 and x > 1400 and x < 1425 and y > 25 and y < 50 then
+--		print("Quit by CROSS")
+--		love.event.quit()
+--	end
 end
 
 function love.keypressed(k)
 	if k == "escape" then
-		print("Quit by ESC")
 		love.event.quit()
 	end
 end
